9c1c24deea06760a6a272dee809e72a22d052203
wanwang2 <1053400545@qq.com>
2017-05-08 23:01:36 +0800

    ...

--- wanwan-level-symbol/src/main/java/org/wanwanframework/level/symbol/PieceController.java
+++ wanwan-level-symbol/src/main/java/org/wanwanframework/level/symbol/PieceController.java
@@ -24,10 +24,24 @@ public class PieceController {
 	
 	public void readStructure() {
 		Map<String, String> map = configs[0];
+		Map<String, String> templateMap = configs[1];
 		for (String key: map.keySet()) {
-			content += map.get(key);
+			String line = map.get(key);
+			String template = readTemplate(templateMap, "->");
+			content += readSymbol(line, "->", template);
 		}
 		Log.log("content:\r\n" + content);
+	}
+	
+	public String readTemplate(Map<String, String> map, String key) {
+		return map.get(key);
+	}
+	
+	public String readSymbol(String line, String key, String template) {
+		if(line.contains(key)) {
+			line = line.replaceAll(key, template);
+		}
+		return line;
 	}
 	
 	public static void main(String[] args) {
